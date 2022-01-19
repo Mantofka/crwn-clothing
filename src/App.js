f7117422b1a28af2c62fed5b9e1e80914a92c2a3
Mantas Petrauskas <mantofka@Mantass-MacBook-Air.local>
2022-01-19 18:05:31 +0200

    Added Routes with the new V6 React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return <div>Hats PAGE</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -49,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, size, id, linkUrl }) => (
-          <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}/>
+        {this.state.sections.map(({ id, ...restProps }) => (
+          <MenuItem key={id} {...restProps} />
         ))}
       </div>
     );
