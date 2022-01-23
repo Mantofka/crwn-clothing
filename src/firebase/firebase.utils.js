ae6f1a320e9963a21710b23c5a49627f032ccec9
Mantas Petrauskas <mantofka@Mantass-MacBook-Air.local>
2022-01-23 21:39:52 +0200

    Implemented firebase utils together with google sign-in and sign-up with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const config = {
   apiKey: "AIzaSyArNNMJ-hwhi3Wkad9A9fRaM8J4Y9Z0JjU",
@@ -10,6 +10,32 @@ const config = {
   messagingSenderId: "469562044447",
   appId: "1:469562044447:web:c7215476ae0200514ca52a",
   measurementId: "G-97QPJHG1VM",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("Error while creating user", error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
