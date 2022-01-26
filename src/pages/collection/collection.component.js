d09dfb8203b7eccd3e3ff258c9cb003d86884259
Mantas Petrauskas <mantofka@Mantass-MacBook-Air.local>
2022-01-26 22:17:35 +0200

    Moved our directory and shop data into its own reducer

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -7,10 +7,16 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 import "./collection.styles.scss";
 
 const CollectionPage = ({ params, collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
+
   return (
-    <div className='category'>
-      <h2>CATEGORY PAGE</h2>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
